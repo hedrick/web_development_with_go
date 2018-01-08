90e88ba2874edd78ef532b7e133264d8aa36ed01
Scott Hedrick <me@scottshedrick.com>
2018-01-08 11:47:06 -0600

    added append functions for template path and extensions and refactored NewView calls to reflect shortened filepaths - complete 7.5

--- controllers/users.go
+++ controllers/users.go
@@ -21,7 +21,7 @@ type SignupForm struct {
 // NewUsers - returns a Users Struct
 func NewUsers() *Users {
 	return &Users{
-		NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
+		NewView: views.NewView("bootstrap", "users/new"),
 	}
 }
 
--- views/view.go
+++ views/view.go
@@ -15,12 +15,16 @@ type View struct {
 var (
 	// LayoutDir - directory for Globbing
 	LayoutDir = "views/layouts/"
-	//TemplateExt - extension for Globbing
+	// TemplateDir - directory for templates
+	TemplateDir = "views/"
+	// TemplateExt - extension for Globbing
 	TemplateExt = ".gohtml"
 )
 
 // NewView to handle logic for each template view
 func NewView(layout string, files ...string) *View {
+	addTemplatePath(files)
+	addTemplateExt(files)
 	files = append(files, layoutFiles()...)
 	t, err := template.ParseFiles(files...)
 	if err != nil {
@@ -50,4 +54,26 @@ func layoutFiles() []string {
 		panic(err)
 	}
 	return files
+}
+
+// addTemplatePath takes in a slice of strings representing
+// file paths for templates and prepends TemplateDir to each
+// string in the slice
+// {"home" would result in {"views/home}
+func addTemplatePath(files []string) {
+	for i, f := range files {
+		files[i] = TemplateDir + f
+	}
+}
+
+// addTemplateExt takes in a slice of strings
+// representing file paths for templates and it appends
+// the TemplateExt extension to each string in the slice
+//
+// Eg the input {"home"} would result in the output
+// {"home.gohtml"} if TemplateExt == ".gohtml"
+func addTemplateExt(files []string) {
+	for i, f := range files {
+		files[i] = f + TemplateExt
+	}
 }
