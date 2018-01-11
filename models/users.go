1f2586c740210f8c6af889938beca32521cd402f
Scott Hedrick <me@scottshedrick.com>
2018-01-11 14:04:13 -0600

    added remember validators and errors - completes 12.9 (and Ch 12)

--- models/users.go
+++ models/users.go
@@ -42,6 +42,14 @@ var (
 	// ErrPasswordRequired is returned when a create is attempted
 	// without a user password provided.
 	ErrPasswordRequired = errors.New("models: password is required")
+	// ErrRememberRequired is returned when a create or update
+	// is attempted without a user remember token hash
+	ErrRememberRequired = errors.New("models: remember token " +
+		"is required")
+	// ErrRememberTooShort is returned when
+	// remember token ! => 32 bytes
+	ErrRememberTooShort = errors.New("models: remember token " +
+		"must be at least 32 bytes")
 
 	userPwPepper = "kffphrhrrrr"
 )
@@ -164,7 +172,9 @@ func (uv *userValidator) Create(user *User) error {
 		uv.bcryptPassword,
 		uv.passwordHashRequired,
 		uv.setRememberIfUnset,
+		uv.rememberMinBytes,
 		uv.hmacRemember,
+		uv.rememberHashRequired,
 		uv.normalizeEmail,
 		uv.requireEmail,
 		uv.emailFormat,
@@ -187,7 +197,9 @@ func (uv *userValidator) Update(user *User) error {
 		uv.passwordMinLength,
 		uv.bcryptPassword,
 		uv.passwordHashRequired,
+		uv.rememberMinBytes,
 		uv.hmacRemember,
+		uv.rememberHashRequired,
 		uv.normalizeEmail,
 		uv.requireEmail,
 		uv.emailFormat,
@@ -478,6 +490,27 @@ func (uv *userValidator) passwordRequired(user *User) error {
 func (uv *userValidator) passwordHashRequired(user *User) error {
 	if user.PasswordHash == "" {
 		return ErrPasswordRequired
+	}
+	return nil
+}
+
+func (uv *userValidator) rememberMinBytes(user *User) error {
+	if user.Remember == "" {
+		return nil
+	}
+	n, err := rand.NBytes(user.Remember)
+	if err != nil {
+		return err
+	}
+	if n < 32 {
+		return ErrRememberTooShort
+	}
+	return nil
+}
+
+func (uv *userValidator) rememberHashRequired(user *User) error {
+	if user.RememberHash == "" {
+		return ErrRememberRequired
 	}
 	return nil
 }
