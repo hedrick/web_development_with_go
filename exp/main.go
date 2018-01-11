4aaaa9c469afc375bc03ce8b067a6387627b8c09
Scott Hedrick <me@scottshedrick.com>
2018-01-11 09:31:05 -0600

    added cookie verification to Login and Create in controllers/users.go - completes 11.8.2

--- exp/main.go
+++ exp/main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strings"
+
+	"../models"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
@@ -38,16 +37,37 @@ func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"dbname=%s sslmode=disable",
 		host, port, user, dbname)
-
-	db, err := gorm.Open("postgres", psqlInfo)
+	us, err := models.NewUserService(psqlInfo)
 	if err != nil {
 		panic(err)
 	}
 
-	defer db.Close()
-	db.LogMode(true)
+	defer us.Close()
+	// db.LogMode(true)
+	us.DestructiveReset()
 
-	db.AutoMigrate(&User{}, &Order{})
+	// user := models.User{
+	// 	Name:     "Michale Scott",
+	// 	Email:    "michael@dundermifflin.com",
+	// 	Password: "bestboss",
+	// }
+	// err = us.Create(&user)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Printf("%+v\n", user)
+	// if user.Remember == "" {
+	// 	panic("Invalid remember token")
+	// }
+
+	// user2, err := us.ByRemember(user.Remember)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Printf("%+v\n", *user2)
+
+	// db.AutoMigrate(&User{}, &Order{})
 
 	// name, email := getInfo()
 	// u := &User{
@@ -59,35 +79,35 @@ func main() {
 	// }
 	// fmt.Printf("%+v\n", u)
 
-	var user User
-	db.Preload("Orders").First(&user)
-	if db.Error != nil {
-		panic(db.Error)
-	}
+	// 	var user User
+	// 	db.Preload("Orders").First(&user)
+	// 	if db.Error != nil {
+	// 		panic(db.Error)
+	// 	}
 
-	fmt.Println("Email:", user.Email)
-	fmt.Println("Number of orders:", len(user.Orders))
-	fmt.Println("Orders:", user.Orders)
-}
+	// 	fmt.Println("Email:", user.Email)
+	// 	fmt.Println("Number of orders:", len(user.Orders))
+	// 	fmt.Println("Orders:", user.Orders)
+	// }
 
-func getInfo() (name, email string) {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("What is your name?")
-	name, _ = reader.ReadString('\n')
-	name = strings.TrimSpace(name)
-	fmt.Println("What is your email?")
-	email, _ = reader.ReadString('\n')
-	email = strings.TrimSpace(email)
-	return name, email
-}
+	// func getInfo() (name, email string) {
+	// 	reader := bufio.NewReader(os.Stdin)
+	// 	fmt.Println("What is your name?")
+	// 	name, _ = reader.ReadString('\n')
+	// 	name = strings.TrimSpace(name)
+	// 	fmt.Println("What is your email?")
+	// 	email, _ = reader.ReadString('\n')
+	// 	email = strings.TrimSpace(email)
+	// 	return name, email
+	// }
 
-func createOrder(db *gorm.DB, user User, amount int, desc string) {
-	db.Create(&Order{
-		UserID:      user.ID,
-		Amount:      amount,
-		Description: desc,
-	})
-	if db.Error != nil {
-		panic(db.Error)
-	}
+	// func createOrder(db *gorm.DB, user User, amount int, desc string) {
+	// 	db.Create(&Order{
+	// 		UserID:      user.ID,
+	// 		Amount:      amount,
+	// 		Description: desc,
+	// 	})
+	// 	if db.Error != nil {
+	// 		panic(db.Error)
+	// 	}
 }
