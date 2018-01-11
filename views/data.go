574c2194b3866287a4b825e2859f93ee1ad0c7bb
Scott Hedrick <me@scottshedrick.com>
2018-01-11 14:39:28 -0600

    added views/data.go for modeling data to pass into view template - completes 13.4

---
+++ views/data.go
@@ -0,0 +1,22 @@
+package views
+
+// Data is the top level structure that views expect data
+// to come in.
+type Data struct {
+	Alert *Alert
+	Yield interface{}
+}
+
+// Alert is used to render Bootstrap Alert messages in templates
+type Alert struct {
+	Level   string
+	Message string
+}
+
+// Constants for AlertLevels for Alert types
+const (
+	AlertLvlError   = "danger"
+	AlertLvlWarning = "warning"
+	AlertLvlInfo    = "info"
+	AlertLvlSuccess = "success"
+)
