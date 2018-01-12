ed25594352924183998fa53fc253fe97bd8edced
Scott Hedrick <me@scottshedrick.com>
2018-01-12 16:21:21 -0600

    added views/galleries/edit.gohtml and registered with routing in main.go. Added galleryByIdto controllers/galleries for convenience purposes. Implemented Edit() in controllers/galleries - completes 14.8.1

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -14,6 +14,7 @@ import (
 type Galleries struct {
 	New      *views.View
 	ShowView *views.View
+	EditView *views.View
 	gs       models.GalleryService
 	r        *mux.Router
 }
@@ -33,6 +34,7 @@ func NewGalleries(gs models.GalleryService, r *mux.Router) *Galleries {
 	return &Galleries{
 		New:      views.NewView("bootstrap", "galleries/new"),
 		ShowView: views.NewView("bootstrap", "galleries/show"),
+		EditView: views.NewView("bootstrap", "galleries/edit"),
 		gs:       gs,
 		r:        r,
 	}
@@ -68,12 +70,41 @@ func (g *Galleries) Create(w http.ResponseWriter, r *http.Request) {
 
 // Show GET /galleries/:id
 func (g *Galleries) Show(w http.ResponseWriter, r *http.Request) {
+	gallery, err := g.galleryByID(w, r)
+	if err != nil {
+		return
+	}
+
+	var vd views.Data
+	vd.Yield = gallery
+	g.ShowView.Render(w, vd)
+}
+
+// Edit /GET /galleries/:id/edit
+func (g *Galleries) Edit(w http.ResponseWriter, r *http.Request) {
+	gallery, err := g.galleryByID(w, r)
+	if err != nil {
+		return
+	}
+	user := context.User(r.Context())
+	if gallery.UserID != user.ID {
+		http.Error(w, "You do not have permission to edit "+
+			"this gallery", http.StatusForbidden)
+		return
+	}
+	var vd views.Data
+	vd.Yield = gallery
+	g.EditView.Render(w, vd)
+}
+
+func (g *Galleries) galleryByID(w http.ResponseWriter,
+	r *http.Request) (*models.Gallery, error) {
 	vars := mux.Vars(r)
 	idStr := vars["id"]
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
 		http.Error(w, "Invalid gallery ID", http.StatusNotFound)
-		return
+		return nil, err
 	}
 	gallery, err := g.gs.ByID(uint(id))
 	if err != nil {
@@ -81,13 +112,9 @@ func (g *Galleries) Show(w http.ResponseWriter, r *http.Request) {
 		case models.ErrNotFound:
 			http.Error(w, "Gallery not found", http.StatusNotFound)
 		default:
-			http.Error(w, "Whoops! Something went wrong.",
-				http.StatusInternalServerError)
+			http.Error(w, "Whoops! Something went wrong.", http.StatusInternalServerError)
 		}
-		return
+		return nil, err
 	}
-
-	var vd views.Data
-	vd.Yield = gallery
-	g.ShowView.Render(w, vd)
+	return gallery, nil
 }
