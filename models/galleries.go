4abf3b183bcc58552a97e1de0f8faedafeda9af0
Scott Hedrick <me@scottshedrick.com>
2018-01-12 10:06:05 -0600

    added errors and validators in models/galleries - completes 14.5.4

--- models/galleries.go
+++ models/galleries.go
@@ -33,11 +33,31 @@ type galleryValidator struct {
 	GalleryDB
 }
 
+type galleryValFn func(*Gallery) error
+
 var _ GalleryDB = &galleryGorm{}
+
+const (
+	// ErrUserIDRequired error
+	ErrUserIDRequired modelError = "models: user ID is required"
+	// ErrTitleRequired error
+	ErrTitleRequired modelError = "models: title is required"
+)
 
 // Create a gallery
 func (gg *galleryGorm) Create(gallery *Gallery) error {
 	return gg.db.Create(gallery).Error
+}
+
+// Create a gallery with validation and then call create to the DB
+func (gv *galleryValidator) Create(gallery *Gallery) error {
+	err := runGalleryValFns(gallery,
+		gv.userIDRequired,
+		gv.titleRequired)
+	if err != nil {
+		return err
+	}
+	return gv.GalleryDB.Create(gallery)
 }
 
 // NewGalleryService returns a new instance of a GalleryService
@@ -49,4 +69,27 @@ func NewGalleryService(db *gorm.DB) GalleryService {
 			},
 		},
 	}
+}
+
+func (gv *galleryValidator) userIDRequired(g *Gallery) error {
+	if g.UserID <= 0 {
+		return ErrUserIDRequired
+	}
+	return nil
+}
+
+func (gv *galleryValidator) titleRequired(g *Gallery) error {
+	if g.Title == "" {
+		return ErrTitleRequired
+	}
+	return nil
+}
+
+func runGalleryValFns(gallery *Gallery, fns ...galleryValFn) error {
+	for _, fn := range fns {
+		if err := fn(gallery); err != nil {
+			return err
+		}
+	}
+	return nil
 }
