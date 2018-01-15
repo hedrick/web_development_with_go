7ddf776102698d4c0746d4acbefded68bef6ee69
Scott Hedrick <me@scottshedrick.com>
2018-01-15 14:08:42 -0600

    added styles.css for more global styling - completes 16.2

--- main.go
+++ main.go
@@ -80,6 +80,11 @@ func main() {
 		requireUserMw.ApplyFn(galleriesC.ImageDelete)).
 		Methods("POST")
 
+	// Assets
+	assetHandler := http.FileServer(http.Dir("./assets/"))
+	assetHandler = http.StripPrefix("/assets/", assetHandler)
+	r.PathPrefix("/assets/").Handler(assetHandler)
+
 	// Image routes
 	imageHandler := http.FileServer(http.Dir("./images/"))
 	r.PathPrefix("/images/").Handler(http.StripPrefix("/images/", imageHandler))
